54ae148671f77bc7c5ed60dd217514cdbc35cf98
Cassie Jones <72354898+hey-cassie@users.noreply.github.com>
2021-06-29 17:33:36 +0000

    Tests 1-9 complete

--- rover.js
+++ rover.js
@@ -1,5 +1,16 @@
 class Rover {
-   // Write code here!
+   constructor(position, mode = 'NORMAL', generatorWatts = 110) {
+      this.position = position;
+      this.mode = mode;
+      this.generatorWatts = generatorWatts;
+   }
+   receiveMessage(message){
+     let response = {
+      message: message.name,
+      results: message.commands
+     }
+     return response;
+   }
 }
 
 module.exports = Rover;
--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -8,6 +8,25 @@ const Command = require('../command.js');
 
 describe("Rover class", function() {
 
-  // 7 tests here!
+  it("constructor sets position and default values for mode and generatorWatts", function() {
+    let rover = new Rover(98382);
+    expect(rover.position).toEqual(98382);
+    expect(rover.mode).toEqual('NORMAL');
+    expect(rover.generatorWatts).toEqual(110);
+  });
+
+  it("response returned by receiveMessage contains name of message", function() {
+    let rover = new Rover(98382);
+    let newMessage = new Message('New message!');
+    expect(rover.receiveMessage(newMessage).message).toEqual('New message!');
+  });
+
+    it("response returned by receiveMessage includes two results if two commands are sent in the message", function() {
+    let rover = new Rover(98382);
+    let commands = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
+    let newMessage = new Message('New message!', commands);
+    expect(rover.receiveMessage(newMessage).results.length).toEqual(commands.length);
+  });
 
 });
+
